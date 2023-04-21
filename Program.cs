f36a6ce7a41bc52ff35181a771f2c34c78726158
LeondeHaas <leonthedragonslayer123@gmail.com>
2023-04-21 11:31:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BlackjackProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
