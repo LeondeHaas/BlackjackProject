7cbee7a9e1c3891e8b45b6a0b7e9fbc312e3af7d
LeondeHaas <leonthedragonslayer123@gmail.com>
2023-04-21 13:05:00 +0200

    a little more progress

---
+++ Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Card
+{
+    protected string cardName;
+    protected string cardType;
+    protected string cardValue;
+
+  
+
+    public Card(string cardName, string cardType, string cardValue) {
+
+    }
+}
+}
